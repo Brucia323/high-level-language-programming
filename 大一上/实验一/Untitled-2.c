7a5f18cfea10303ef1ca6e3e82bc891027748355
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2020-05-07 21:10:36 +0800

    Add files via upload

---
+++ 大一上/实验一/Untitled-2.c
@@ -0,0 +1,6 @@
+/*本题要求编写程序，输出一个短句"Welcome to You!"*/
+#include <stdio.h>
+int main(void)
+{
+    printf("Welcome to You!");
+}
---
+++ 大一上/实验一/Untitled-3.c
@@ -0,0 +1,6 @@
+/*本题要求编写程序，输出一个短句"What is a computer?"*/
+#include <stdio.h>
+int main(void)
+{
+    printf("What is a computer?");
+}
